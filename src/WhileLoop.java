b14c448bc7ce0a6d25eb7a1f97d32e240aa02ed7
cmroman <cristinamroman@gmail.com>
2017-02-02 14:52:50 -0600

    Initial WhileLoop Test

---
+++ src/WhileLoop.java
@@ -0,0 +1,15 @@
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numberOfLoops = 1;
+		while(numberOfLoops < 11)
+		{
+			System.out.println(numberOfLoops);
+			numberOfLoops = numberOfLoops + 1; 
+		}
+		System.out.println("You're done");
+	}
+
+}
